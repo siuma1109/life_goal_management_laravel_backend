d6fd55fa4f6e583a6b4714cf0ec591f9ffdea43d
siuma1109 <matsunkit1109@gmail.com>
2025-04-20 22:52:25 +0800

    Project CRUD

---
+++ app/Http/Controllers/Api/V1/ProjectController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::query()
+            ->where('user_id', Auth::id())
+            ->get();
+
+        return response()->json($projects);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $project = Project::create([
+            'name' => $request->name,
+            'user_id' => Auth::id(),
+        ]);
+
+        return response()->json($project);
+    }
+
+    public function show(Project $project)
+    {
+        if (!$project->user_id === Auth::id()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        return response()->json($project);
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        $project->update($request->all());
+        return response()->json($project);
+    }
+
+    public function destroy(Project $project)
+    {
+        if ($project->user_id !== Auth::id()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $project->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('projects', ProjectController::class)->middleware('auth:sanctum');
