cc52d649bd784e1833da2d70f4e26e2f16b67bbd
siuma1109 <matsunkit1109@gmail.com>
2025-04-20 21:51:09 +0800

    Login feature

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken("API TOKEN")->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
